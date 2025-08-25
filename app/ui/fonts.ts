e96eae503b6a7719c97d960e23c27691e4c6cc5d
Daniel Morillo <danifromecuador@gmail.com>
2025-08-24 22:22:45 -0500

    Create a Next.js app

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import {Inter} from 'next/font/google'
+import { Lusitana } from 'next/font/google'
+import { Caveat } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin']})
+export const lusitana = Lusitana({weight: ['400']})
+export const caveat = Caveat({weight: ['400']})
