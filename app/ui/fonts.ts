2074a7b309814dae38d8e7c491fc03e3132181de
Daniel Morillo <danifromecuador@gmail.com>
2025-08-24 22:34:42 -0500

    fix: add missing font subsets to resolve build error

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,13 @@
-import {Inter} from 'next/font/google'
+import { Inter } from 'next/font/google'
 import { Lusitana } from 'next/font/google'
 import { Caveat } from 'next/font/google'
 
-export const inter = Inter({ subsets: ['latin']})
-export const lusitana = Lusitana({weight: ['400']})
-export const caveat = Caveat({weight: ['400']})
+export const inter = Inter({ subsets: ['latin'] })
+export const lusitana = Lusitana({ 
+  subsets: ['latin'],
+  weight: ['400', '700']
+})
+export const caveat = Caveat({
+  subsets: ['latin'],
+  weight: ['400']
+})
